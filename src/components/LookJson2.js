908529053e2387a63fa1d6c85bf312a8df4bfbb0
jiho <wjeeho@gmail.com>
2022-10-04 23:25:37 +0900

    LookJson update

--- src/components/LookJson2.js
+++ src/components/LookJson2.js
@@ -3,8 +3,11 @@ import React from "react"
 const LookJson = ({ jsonObj2 }) => {
     return (
         <>
-            <div className="item">{jsonObj2[0]}</div>
-            <div className="item"> {jsonObj2[1]}</div>
+            {/* <div className="item">{jsonObj2[0]}</div>
+            <div className="item"> {jsonObj2[1]}</div> */}
+            <div>
+            {jsonObj2[0]} : {jsonObj2[1]}
+            </div>
         </>
     )
 }
