6f191175300079d930f66cfb66cc538c0731b7e2
jiho <wjeeho@gmail.com>
2022-10-19 16:41:38 +0900

    design update

--- src/components/LookJson2.js
+++ src/components/LookJson2.js
@@ -3,7 +3,7 @@ import React from "react"
 const LookJson = ({ jsonObj2 }) => {
     return (
         <>
-            <div className="item">{jsonObj2[0]}</div>
+            <div className="item formName">{jsonObj2[0]}</div>
             <div className="item prevent-overflow"> {jsonObj2[1]}</div>
         </>
     )
